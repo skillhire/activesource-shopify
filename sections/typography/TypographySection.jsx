5701357dfedc396b58bbf812e01c1b56bd0e3b09
Michael <itsmichaeldiego@gmail.com>
2023-10-31 17:15:43 -0500

    Add basic Typography

---
+++ sections/typography/TypographySection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const TypographySection = (props) => {
+  return (
+    <Box>
+      <Typography variant="h1">Big Heading (SemiBold)</Typography>
+      <Typography variant="h2">Heading (SemiBold)</Typography>
+    </Box>
+  );
+};
+
+export default TypographySection;
