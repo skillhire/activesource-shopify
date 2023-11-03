d2f510fdab0dc7a13b9aeb20d4e2c4c67a5b9d2a
Michael <itsmichaeldiego@gmail.com>
2023-11-03 14:36:58 -0500

    Temp: Hero fixes

--- sections/home/SectionHeroPrimary.jsx
+++ sections/home/SectionHeroPrimary.jsx
@@ -11,12 +11,17 @@ const SectionHeroPrimary = (props) => {
   };
 
   return (
-    <Container maxWidth="xl">
-      <Stack sx={sx.root} spacing={2}>
-        <Typography variant="h2" maxWidth={500}>{home?.title}</Typography>
-        <Typography variant="body1">{home?.subtitle}</Typography>
+    <Container maxWidth="lg" sx={sx.root}>
+      <Stack sx={sx.container} spacing={2}>
+        <Typography variant="h2" maxWidth={500} color="primary.contrastText">
+          {home?.title}
+        </Typography>
+        <Typography variant="body1" color="primary.contrastText">
+          {home?.subtitle}
+        </Typography>
         <Box>
-          <Button variant="contained" color="secondary">Explore Products</Button>
+          <Button
+            variant="contained" color="secondary">Explore Products</Button>
         </Box>
       </Stack>
     </Container>
@@ -27,7 +32,14 @@ export default SectionHeroPrimary;
 
 const sx = {
   root: {
-    mt: {
+    backgroundColor: "primary.main",
+    minHeight: {
+      sm: "calc(100vh - 64px)",
+      xs: "calc(100vh - 50px)",
+    },
+  },
+  container: {
+    py: {
       sm: "80px",
       xs: 0,
     },
