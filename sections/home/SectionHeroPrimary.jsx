bf7347bd979dcdf3d02c60734987db08be1249c4
Michael <itsmichaeldiego@gmail.com>
2023-11-04 17:06:34 -0500

    Improve Layout

--- sections/home/SectionHeroPrimary.jsx
+++ sections/home/SectionHeroPrimary.jsx
@@ -11,20 +11,23 @@ const SectionHeroPrimary = (props) => {
   };
 
   return (
-    <Container maxWidth="lg" sx={sx.root}>
-      <Stack sx={sx.container} spacing={2}>
-        <Typography variant="h2" maxWidth={500} color="primary.contrastText">
-          {home?.title}
-        </Typography>
-        <Typography variant="body1" color="primary.contrastText">
-          {home?.subtitle}
-        </Typography>
-        <Box>
-          <Button
-            variant="contained" color="secondary">Explore Products</Button>
-        </Box>
-      </Stack>
-    </Container>
+    <Box sx={sx.root}>
+      <Container maxWidth="lg">
+        <Stack spacing={2}>
+          <Typography variant="h2" maxWidth={500} color="primary.contrastText">
+            {home?.title}
+          </Typography>
+          <Typography variant="body1" color="primary.contrastText">
+            {home?.subtitle}
+          </Typography>
+          <Box>
+            <Button variant="contained" color="secondary">
+              Explore Products
+            </Button>
+          </Box>
+        </Stack>
+      </Container>
+    </Box>
   );
 };
 
@@ -32,16 +35,13 @@ export default SectionHeroPrimary;
 
 const sx = {
   root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: "primary.main",
     minHeight: {
       sm: "calc(100vh - 64px)",
       xs: "calc(100vh - 50px)",
-    },
-  },
-  container: {
-    py: {
-      sm: "80px",
-      xs: 0,
     },
   },
 };
