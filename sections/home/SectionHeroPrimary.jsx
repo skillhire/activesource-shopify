d03a67227b91fd8ad85dddb14f52963f3aa97df7
Michael <itsmichaeldiego@gmail.com>
2023-11-06 15:55:55 -0500

    Add alt to hero

--- sections/home/SectionHeroPrimary.jsx
+++ sections/home/SectionHeroPrimary.jsx
@@ -24,7 +24,7 @@ const SectionHeroPrimary = (props) => {
               </Button>
             </Box>
           </Stack>
-          <Image src={HeroImage} width={450} />
+          <Image src={HeroImage} alt="Hero Image" width={450} />
         </Box>
       </Container>
     </Box>
