e45bc74d901f2626972d62541913de2b1fd479a0
Michael <itsmichaeldiego@gmail.com>
2023-11-06 17:47:12 -0500

    Move helper to utils

--- components/products/ProductCard.jsx
+++ components/products/ProductCard.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useClickOrDrag } from "hooks";
 import { Box, CardActionArea, Typography, Stack } from "@mui/material";
 
-import { truncate, formatCurrency } from "utils";
+import { truncate, formatPrice } from "utils";
 import { Image } from "components";
 import ColorOption from "components/variants/ColorOption";
 
@@ -40,10 +40,10 @@ const ProductCard = ({ product, handleClick }) => {
         sx={sx.contentActionArea}
       >
         {image && (
-          <Image src={image} alt={product.title} objectFit="cover" />
+          <Image src={image} alt={product.title} objectFit="contain" />
         )}
       </CardActionArea>
-      <Stack px={2} py={1} spacing={1}>
+      <Stack px={2} py={2} spacing={0.5}>
         <Typography variant="subtitle1" size="small" color="textPrimary" sx={sx.title}>
           {truncate(product?.title)}
         </Typography>
@@ -55,9 +55,11 @@ const ProductCard = ({ product, handleClick }) => {
             handleClick={handleColorClick}
           />
         ))}
-        <Typography variant="button" color="textPrimary" sx={sx.price}>
-          {formatCurrency(product?.priceRange?.minVariantPrice?.amount)}
-        </Typography>
+        {product && (
+          <Typography variant="button" color="textPrimary" sx={sx.price}>
+            {formatPrice(product.priceRange?.minVariantPrice.amount, product.priceRange?.maxVariantPrice.amount)}
+          </Typography>
+        )}
       </Stack>
     </Box>
   );
