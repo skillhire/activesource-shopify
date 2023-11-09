5b685803f0c4a23f7237cb62ce6b5963041c6316
Michael <itsmichaeldiego@gmail.com>
2023-11-09 15:17:33 -0500

    Add icons and images

--- sections/home/SectionHowItWorks.jsx
+++ sections/home/SectionHowItWorks.jsx
@@ -1,13 +1,34 @@
 import Image from "next/image";
-import { Stack, Chip, Container, Grid, Typography } from "@mui/material";
+import { Stack, Chip, Container, Grid, Typography, Box, IconButton } from "@mui/material";
 
-import PickProduct from "assets/pick-product@2x.png";
+import DesignImage from "assets/upload-design@2x.png";
+import PickProductImage from "assets/pick-product@2x.png";
+import OrderProductImage from "assets/order-product@2x.png";
+import DesignIcon from "assets/pencil-icon.svg";
+import PickProductIcon from "assets/cursor-icon.svg";
+import OrderProductIcon from "assets/cart-icon.svg";
 
-const ITEMS = [{
-  title: "Pick a Product",
-  description: "Choose from our wide range of products",
-  image: PickProduct,
-}]
+
+const ITEMS = [
+  {
+    title: "Pick a Product",
+    description: "Choose from our wide range of products",
+    image: PickProductImage,
+    icon: PickProductIcon,
+  },
+  {
+    title: "Upload your Design",
+    description: "Easily add your designs to a wide range of products using our free tools",
+    image: DesignImage,
+    icon: DesignIcon,
+  },
+  {
+    title: "Order the Product",
+    description: "Relax while we handle the fulfillment and shipping of your order",
+    image: OrderProductImage,
+    icon: OrderProductIcon,
+  }
+]
 
 const SectionFeaturedCollection = () => {
   return (
@@ -19,26 +40,40 @@ const SectionFeaturedCollection = () => {
         </Typography>
       </Stack>
       <Stack>
-        {ITEMS.map((item, i) => (
-          <Grid container key={item.title}>
-            <Grid item xs={12} sm={6}>
-              <Stack>
-                <Typography variant="body1">STEP {i >= 10 ? i + 1 : `0${i + 1}`}</Typography>
-                <Typography variant="h3">
-                  {item.title}
-                </Typography>
-                <Typography variant="body1">
-                  {item.description}
-                </Typography>
-              </Stack>
-            </Grid>
-            <Grid item xs={12} sm={6} sx={sx.imageContainer}>
-              <Image src={item.image} alt="Hero Image 2" style={sx.image} />
-            </Grid>
-          </Grid>
-        ))}
+        {ITEMS.map((item, i) => {
+          let itemContainerClass = { ...sx.itemContainer };
+          if (i % 2 !== 0) {
+            itemContainerClass = { ...itemContainerClass, ...sx.itemContainerReversed }
+          }
+          return (
+            <Box
+              key={item.title}
+              sx={itemContainerClass}
+            >
+              <Box sx={sx.item}>
+                <Stack spacing={1}>
+                  <Box>
+                    <IconButton disabled sx={sx.iconButton} >
+                      <Image responsive="true" src={item.icon} alt={`${item.title} Icon`} width={24} height={24} />
+                    </IconButton>
+                  </Box>
+                  <Typography variant="body1">STEP {i >= 10 ? i + 1 : `0${i + 1}`}</Typography>
+                  <Typography variant="h3">
+                    {item.title}
+                  </Typography>
+                  <Typography variant="body1" maxWidth={380}>
+                    {item.description}
+                  </Typography>
+                </Stack>
+              </Box>
+              <Box sx={sx.imageContainer}>
+                <Image src={item.image} alt={`${item.title} Image`} style={sx.image} />
+              </Box>
+            </Box>
+          )
+        })}
       </Stack>
-    </Container>
+    </Container >
   );
 };
 
@@ -53,6 +88,39 @@ const sx = {
     alignItems: "center",
     textAlign: "center",
   },
+  iconButton: {
+    p: 1,
+    background: (theme) => `${theme.palette.tertiary.main} !important`
+  },
   image: { objectFit: "contain", width: "75%", height: "auto" },
-  imageContainer: { position: "relative" },
+  imageContainer: {
+    position: "relative",
+    "@media (max-width: 600px)": {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+  itemContainer: {
+    my: 5,
+    display: "flex",
+    flexDirection: "row",
+    "@media (max-width: 600px)": {
+      flexDirection: "column",
+    },
+  },
+  itemContainerReversed: {
+    flexDirection: "row-reverse",
+  },
+  item: {
+    my: 5,
+    px: 4,
+    flex: 1,
+    "@media (max-width: 600px)": {
+      display: "flex",
+      textAlign: "center",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  }
 };
