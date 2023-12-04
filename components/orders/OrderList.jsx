f12f87787514cd9774ec1b3b8d14aeefee9f8c73
Michael <itsmichaeldiego@gmail.com>
2023-12-04 17:08:01 -0600

    Orders v1

--- components/orders/OrderList.jsx
+++ components/orders/OrderList.jsx
@@ -1,13 +1,26 @@
-import React, { useEffect } from "react";
-import { Box, List } from "@mui/material";
+import React, { useEffect, useCallback } from "react";
+import {
+  Box,
+  Stack,
+  Paper,
+  Button,
+  Typography,
+  Table,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableContainer,
+  CircularProgress,
+} from '@mui/material';
+
 import { OrderItem } from "components";
 import { useAuth, useOrders } from "hooks";
-import { getCookie } from "cookies-next";
 
 const OrderList = () => {
   const { accessToken } = useAuth();
 
-  const { orders, fetchCustomerOrders } = useOrders();
+  const { orders, loading, fetchCustomerOrders } = useOrders();
 
   const handleClick = (order) => {
     window.open(order?.statusUrl, "_blank");
@@ -19,15 +32,80 @@ const OrderList = () => {
     }
   }, [accessToken]);
 
+  const renderContent = useCallback(() => {
+    if (orders?.length > 0) {
+      return (
+        <Table sx={{ minWidth: "100%" }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography variant="body2">Product</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="body2">Order Number</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="body2">Date</Typography>
+              </TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders?.map((order) => (
+              <OrderItem key={order.id} order={order} handleClick={handleClick} />
+            ))}
+          </TableBody>
+        </Table>
+      )
+    }
+    return (
+      <Stack sx={sx.emptyMessage} spacing={2}>
+        <Typography variant="h4">You have no orders yet</Typography>
+        <Typography variant="body1">Start making your own personalized collections</Typography>
+        <Box pt={5}>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            sx={sx.button}
+            onClick={handleClick}
+          >
+            Explore Products
+          </Button>
+        </Box>
+      </Stack>
+    )
+  }, [])
+
   return (
     <Box>
-      <List>
-        {orders?.map((order) => (
-          <OrderItem key={order.id} order={order} handleClick={handleClick} />
-        ))}
-      </List>
-    </Box>
+      <TableContainer component={Paper} sx={sx.container}>
+        {loading && (
+          <Box sx={{ ...sx.container, ...sx.loader }}>
+            <CircularProgress color="primary" />
+          </Box>
+        )}
+        {!loading && renderContent()}
+      </TableContainer>
+    </Box >
   );
 };
+
+const sx = {
+  container: {
+    minHeight: 380,
+  },
+  emptyMessage: {
+    padding: 12,
+    alignItems: "center",
+  },
+  loader: {
+    flex: 1,
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+}
 
 export default OrderList;
