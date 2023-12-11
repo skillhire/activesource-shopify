6f48ce3d028bddfe0eb10c82e2c506d7cdb07997
Michael <itsmichaeldiego@gmail.com>
2023-12-11 11:34:10 -0600

    Finnesse

--- components/AccountDetails.jsx
+++ components/AccountDetails.jsx
@@ -1,3 +1,4 @@
+import { useState, useCallback } from "react";
 import {
   Box,
   Stack,
@@ -11,44 +12,66 @@ import {
 } from "@mui/material";
 
 export default function AccountDetails() {
+  const [isEditing, setIsEditing] = useState(false);
+  const handleCancel = useCallback(() => {
+    setIsEditing(false)
+  }, [setIsEditing])
+
+  const handleSubmit = useCallback(() => {
+    // TODO: handleSubmit
+  }, [])
+
+  const handleEdit = useCallback(() => {
+    setIsEditing(true)
+  }, [setIsEditing])
+
   return (
     <Stack sx={sx.root}>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={12} md={4} sx={sx.labelContainer}>
-          <InputLabel htmlFor="first-name" sx={sx.inputLabel}>First Name: </InputLabel>
+          <InputLabel htmlFor="first-name" sx={sx.inputLabel}>
+            <Typography variant="body1">
+              First Name:
+            </Typography>
+          </InputLabel>
         </Grid>
         <Grid item xs={12} sm={12} md={8}>
-          <TextField
-            id="first-name"
-            name="first-name"
-            size="small"
-            fullWidth
-            sx={sx.input}
-          />
+          {isEditing && <TextField id="first-name" name="first-name" size="small" fullWidth sx={sx.input} />}
+          {!isEditing && (
+            <Typography variant="h5" sx={sx.infoLabel}>
+              ?????
+            </Typography>
+          )}
         </Grid>
         <Grid item xs={12} sm={12} md={4} sx={sx.labelContainer}>
-          <InputLabel htmlFor="last-name" sx={sx.inputLabel}>Last Name: </InputLabel>
+          <InputLabel htmlFor="last-name" sx={sx.inputLabel}>
+            <Typography variant="body1">
+              Last Name:
+            </Typography>
+          </InputLabel>
         </Grid>
         <Grid item xs={12} sm={12} md={8}>
-          <TextField
-            id="last-name"
-            name="last-name"
-            size="small"
-            fullWidth
-            sx={sx.input}
-          />
+          {isEditing && <TextField id="last-name" name="last-name" size="small" fullWidth sx={sx.input} />}
+          {!isEditing && (
+            <Typography variant="h5" sx={sx.infoLabel}>
+              ?????
+            </Typography>
+          )}
         </Grid>
         <Grid item xs={12} sm={12} md={4} sx={sx.labelContainer}>
-          <InputLabel htmlFor="email" sx={sx.inputLabel}>Email Address: </InputLabel>
+          <InputLabel htmlFor="email-address" sx={sx.inputLabel}>
+            <Typography variant="body1">
+              Email Address:
+            </Typography>
+          </InputLabel>
         </Grid>
         <Grid item xs={12} sm={12} md={8}>
-          <TextField
-            id="email"
-            name="email"
-            size="small"
-            fullWidth
-            sx={sx.input}
-          />
+          {isEditing && <TextField id="email-address" name="email-address" size="small" fullWidth sx={sx.input} />}
+          {!isEditing && (
+            <Typography variant="h5" sx={sx.infoLabel}>
+              ?????
+            </Typography>
+          )}
         </Grid>
       </Grid>
       <Box mt={2}>
@@ -69,29 +92,48 @@ export default function AccountDetails() {
           }
         />
       </Box>
-      <Stack alignItems="end">
-        <Button
-          sx={sx.button}
-          fullWidth
-          size="small"
-          color="secondary"
-          variant="contained"
-        // onClick={handleSubmit}
-        >
-          Save Changes
-        </Button>
+      <Stack alignItems="end" mt={2}>
+        {isEditing && (
+          <Stack direction="row" spacing={2}>
+            <Button
+              sx={sx.button}
+              size="small"
+              color="tertiary"
+              variant="contained"
+              onClick={handleCancel}
+            >
+              Cancel
+            </Button>
+            <Button
+              sx={sx.button}
+              size="small"
+              color="secondary"
+              variant="contained"
+              onClick={handleSubmit}
+            >
+              Save Changes
+            </Button>
+          </Stack>
+        )}
+        {!isEditing && (
+          <Button
+            sx={sx.button}
+            size="small"
+            color="tertiary"
+            variant="contained"
+            onClick={handleEdit}
+          >
+            Edit
+          </Button>
+        )}
       </Stack>
     </Stack>
   );
 }
 
 const sx = {
-  root: {
-    maxWidth: 500,
-  },
+  root: { maxWidth: 500 },
   button: {
-    my: 2,
-    mt: 4,
     width: "auto",
     minWidth: 200,
   },
@@ -99,15 +141,11 @@ const sx = {
     display: "flex",
     alignItems: "center",
   },
-  inputLabel: {
-    color: "primary.main",
-    fontFamily: "Poppins",
-    fontSize: "20px",
-    fontStyle: "normal",
-    fontWeight: 400,
-    lineHeight: "120%",
+  infoLabel: {
+    minHeight: 46,
+    display: "flex",
+    alignItems: "center",
   },
-  input: {
-    backgroundColor: "background.paper"
-  }
+  inputLabel: { color: "primary.main" },
+  input: { backgroundColor: "background.paper" },
 };
