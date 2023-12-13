1256a186522b58c569367bbfe665be30a634d570
Michael <itsmichaeldiego@gmail.com>
2023-12-13 11:29:05 -0600

    Route back to login if accessToken not defined

--- pages/account/orders.jsx
+++ pages/account/orders.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import { LOGIN_URL } from "constants/navigation";
 import { AccountLayout, OrderList } from "components";
+import { useAuth, useAlerts } from "hooks";
 
 const MyOrders = () => {
+  const router = useRouter();
+  const { accessToken } = useAuth();
+  const { showAlertError } = useAlerts();
+
+  useEffect(() => {
+    if (!accessToken) {
+      showAlertError("Please log in to view this page");
+      router.push(LOGIN_URL);
+    }
+  }, [accessToken]);
+
+  if (!accessToken) {
+    return null;
+  }
+
   return (
     <AccountLayout title="Order History">
       <OrderList />
