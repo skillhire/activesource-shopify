b92798fb97119e3c4ac4281633c0cd617ae100c6
Rami Bitar <rbitar@gmail.com>
2024-06-18 11:58:06 -0400

    Disable tabs on enterprise products (#72)

--- components/products/ProductGrid.jsx
+++ components/products/ProductGrid.jsx
@@ -1,8 +1,5 @@
-import PropTypes from "prop-types";
 import { Grid, Box, CircularProgress, Stack } from "@mui/material";
 import { ProductCard } from "components";
-import { useRouter } from "next/router";
-import { useSegment } from "hooks";
 
 const ProductGrid = ({
   products,
@@ -16,19 +13,15 @@ const ProductGrid = ({
   productUrl,
   ...props
 }) => {
-  const router = useRouter();
-  const { trackProductClicked } = useSegment();
 
   return (
     <Stack alignItems="center" py={2}>
       <Grid container spacing={1} {...props}>
-        {products &&
-          !loading &&
-          products.map((product) => (
-            <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl} key={product.id}>
-              <ProductCard product={product} productUrl={productUrl} />
-            </Grid>
-          ))}
+        {!loading && products?.map((product) => (
+          <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl} key={product.id}>
+            <ProductCard product={product} productUrl={productUrl} />
+          </Grid>
+        ))}
       </Grid>
       {loading && (
         <Box sx={sx.loading}>
